11e476dd684bffee3cc4e1675c99c9e97b6c9a3e
ALittleDaisy <1152594956@qq.com>
2024-07-03 16:47:54 +0800

    补充长按回调的考虑

--- app/src/main/java/com/hsf/multitest/scroll_touch/STouchButton.kt
+++ app/src/main/java/com/hsf/multitest/scroll_touch/STouchButton.kt
@@ -9,6 +9,17 @@ import android.widget.Button
 
 class STouchButton(context: Context, attributeSet: AttributeSet) : Button(context, attributeSet) {
 
+    init {
+        /**
+         * 如果长按按钮，而没有发生大幅度的移动，是会回调OnLongClickListener的
+         * 不过在此之后如果发生了移动，事件就被ScrollView拦截了，还是会导致得到事件ACTION_CANCEL的
+         */
+        setOnLongClickListener {
+            Log.d("Daisy", "还是可以相应长按的")
+            true
+        }
+    }
+
     /**
      * 当STouchButton被包括在可以滑动的ViewGroup中，如果发生饿了滑动，是会发送ACTION_CANCEL事件的
      */
